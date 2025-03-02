a8b81711764c5dbf689614ec283fd80b58b0ca36
Sabbir-75 <kazisabbir4361@gmail.com>
2025-03-02 14:45:27 +0600

    90% complete this website

--- JS/function.js
+++ JS/function.js
@@ -1,59 +1,59 @@
 console.log("Yes")
-function bgColor(id, number){
+function bgColor(id, number) {
     let bgColorChange = document.getElementById(id);
     let randomNumber = Math.random(number) * 5;
     let randoms = Math.floor(randomNumber);
     console.log(randoms);
-    if(randoms === 0){
+    if (randoms === 0) {
         bgColorChange.style.backgroundColor = "#EB5A3C";
     }
-    if(randoms === 1){
+    if (randoms === 1) {
         bgColorChange.style.backgroundColor = "#09122C";
     }
-    if(randoms === 2){
+    if (randoms === 2) {
         bgColorChange.style.backgroundColor = "#4B164C";
     }
-    if(randoms === 3){
+    if (randoms === 3) {
         bgColorChange.style.backgroundColor = "#3674B5";
     }
-    if(randoms === 4){
+    if (randoms === 4) {
         bgColorChange.style.backgroundColor = "#E52020";
     }
-    if(randoms === 5){
+    if (randoms === 5) {
         bgColorChange.style.backgroundColor = "#3D8D7A";
     }
-    if(randoms === 6){
+    if (randoms === 6) {
         bgColorChange.style.backgroundColor = "#FFB200";
     }
-    if(randoms === 7){
+    if (randoms === 7) {
         bgColorChange.style.backgroundColor = "#003092";
     }
-    if(randoms === 8){
+    if (randoms === 8) {
         bgColorChange.style.backgroundColor = "#443627";
     }
-    if(randoms === 9){
+    if (randoms === 9) {
         bgColorChange.style.backgroundColor = "#AC1754";
     }
-    if(randoms === 10){
+    if (randoms === 10) {
         bgColorChange.style.backgroundColor = "#AC1754";
     }
-    
+
 }
 
-    
-function createDate(id){
+
+function createDate(id) {
     let newDate = document.getElementById(id);
-    let obj = {    
+    let obj = {
         month: "long",
         day: "numeric",
         year: "numeric"
     }
     let date1 = new Date();
     let curentDate = date1.toLocaleDateString('en-BD', obj);
-   newDate.innerText = curentDate;
+    newDate.innerText = curentDate;
 }
 
-function days(id){
+function days(id) {
     let newdays = document.getElementById(id);
     let allDayes = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
     let newDate = new Date();
@@ -62,19 +62,19 @@ function days(id){
     newdays.innerText = actualDay
 }
 
-function disable(id){
+function disable(id) {
     let btndisable = document.getElementById(id);
     btndisable.setAttribute("disabled", true);
-  }
+}
 
-function deccress(id){
+function deccress(id) {
     let newCountt = document.getElementById(id);
     let newCount = parseInt(newCountt.innerText);
     newCount -= 1;
     newCountt.innerText = newCount;
 }
 
-function incress(id){
+function incress(id) {
     let newCountt = document.getElementById(id);
     let newCount = parseInt(newCountt.innerText);
     newCount += 1;
@@ -82,7 +82,21 @@ function incress(id){
 }
 
 
+function addection(id, title) {
+    let newDateTitle = new Date();
+    let dateTitle = newDateTitle.toLocaleTimeString();
+    let createtitle = document.getElementById(title).innerText;
+    createtitle.innerText = dateTitle;
+    let creatSection = document.getElementById(id);
+    let div = document.createElement("div");
+    div.innerHTML = `
+   <p class="p-2 text-start bg-gray rounded-lg text-base font-normal text-paragraph">You have Complete The Task <span class="font-medium">${createtitle}</span> at<span class="font-medium"> ${dateTitle}</span></p>
+    `
+    creatSection.appendChild(div);
+}
 
 
-    
+
+
+
 
--- JS/main.js
+++ JS/main.js
@@ -46,5 +46,30 @@ document.getElementById("btn_6").addEventListener("click", function(){
   deccress("decr");
 })
 
+document.getElementById("btn_1").addEventListener("click", function(){
+  addection("new_section", "title_1");
+  
+})
+document.getElementById("btn_2").addEventListener("click", function(){
+  addection("new_section", "title_2");
+  
+})
+document.getElementById("btn_3").addEventListener("click", function(){
+ addection("new_section", "title_3");
+  
+})
+document.getElementById("btn_4").addEventListener("click", function(){
+ addection("new_section", "title_4");
+  
+})
+document.getElementById("btn_5").addEventListener("click", function(){
+ addection("new_section", "title_5");
+  
+})
+document.getElementById("btn_6").addEventListener("click", function(){
+  addection("new_section", "title_6");
+  
+})
+
 
 
